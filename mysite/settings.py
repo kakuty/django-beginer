478a7720a484205ac4276e1307e060abcbf89bcb
hixh <im2lazzzzzy@gmail.com>
2017-08-10 15:50:54 +0700

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aj55ra%by16+!87nn&y+33$92jc-qzn)i^w5saa*mz16lfujrp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'kakuty.pythonanywhere.com']
 
 
 # Application definition
