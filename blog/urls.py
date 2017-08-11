f6e1e2d9c7720c8b48e4d56c9760cf50ca145ac7
hixh <im2lazzzzzy@gmail.com>
2017-08-11 07:32:27 +0700

    Add login

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
